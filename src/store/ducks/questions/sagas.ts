095e9592c14fc92f978e0f25f4afc4fb3e11b6b0
Jeniffer Carvalho <jenicarvalho@live.com>
2020-12-08 00:33:08 -0300

    removed unused action

--- src/store/ducks/questions/sagas.ts
+++ src/store/ducks/questions/sagas.ts
@@ -5,12 +5,7 @@ import { QuestionsResponse } from "./types"
 
 import QuestionsService from "../../../services/questions-service"
 
-interface Action {
-  type: string
-  payload: QuestionsResponse
-}
-
-export function* getQuestions(action: Action) {
+export function* getQuestions() {
   try {
     const response: QuestionsResponse = yield call(QuestionsService.getQuestions)
     yield put(loadQuestionsSuccess(response))
