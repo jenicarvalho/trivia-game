d8c4d2568674d5abc9733ba460e43d33637b156e
Jeniffer Carvalho <jenicarvalho@live.com>
2023-06-25 23:26:25 -0300

    improved styles

--- src/store/ducks/rootSaga.ts
+++ src/store/ducks/rootSaga.ts
@@ -3,12 +3,8 @@ import { all, takeLatest } from "redux-saga/effects"
 import { QuestionsTypes } from "./questions/types"
 import { getQuestions } from "./questions/sagas"
 
-import { UserTypes } from "./user/types"
-import { getUser } from "./user/sagas"
-
 export default function* rootSaga() {
   return yield all([
-    takeLatest(QuestionsTypes.GET_QUESTIONS_REQUEST, getQuestions),
-    takeLatest(UserTypes.GET_USER_REQUEST, getUser),
+    takeLatest(QuestionsTypes.GET_QUESTIONS_REQUEST, getQuestions)
   ])
 }
--- src/store/ducks/user/actions.ts
+++
@@ -1,6 +0,0 @@
-import { action } from "typesafe-actions"
-import { UserTypes, User } from "./types"
-
-export const loadUserRequest = () => action(UserTypes.GET_USER_REQUEST)
-export const loadUserSuccess = (data: User[]) => action(UserTypes.GET_USER_SUCCESS, data)
-export const loadUserFailure = () => action(UserTypes.GET_USER_FAILURE)
