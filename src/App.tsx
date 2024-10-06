61501b349427cb427cd84d1976f945cc16fdeba1
Jeniffer Carvalho <carvalho@Jeniffers-MacBook-Pro.local>
2024-10-06 19:02:27 -0300

    added routes and select sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Main } from './global'
 import CategorySelection from './pages/CategorySelection'
 import Home from './pages/Home'
 
 function App() {
-
   return (
     <Main>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/choose-a-category" element={<CategorySelection />} />
+        </Routes>
+      </Router>
     </Main>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,16 +3,25 @@ import useSound from 'use-sound';
 import playSound from '../../assets/audio/play.wav';
 import { Title } from "../../components/Title";
 import { Subtitle } from "../../components/Subtitle";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   const [play] = useSound(playSound);
+
+  const playAndGo = () => {
+    play();
+    setTimeout(() => {
+      navigate('/choose-a-category');
+    }, 1000);
+  }
 
   return (
     <Container>
       <Title>Trivia Game</Title>
       <Subtitle>Think you're a trivia master? Can you score <span>100%</span> and prove it? Test your knowledge now! 🧠 🔥</Subtitle>
 
-      <Play onClick={play}>PLAY</Play>
+      <Play onClick={playAndGo}>PLAY</Play>
     </Container>
   );
 }
