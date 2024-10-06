f196f2f0d818acadb23457baec68ebbc4eec745a
Jeniffer Carvalho <jenicarvalho@live.com>
2024-10-05 21:21:05 -0300

    created home page

---
+++ src/global.ts
@@ -0,0 +1,32 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --black: #161616;
+    --white: #ffffff;
+    --green: #dcf348;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Manrope", sans-serif;
+    font-weight: 200;
+    background-color: var(--black);
+    color: var(--white);
+  }
+
+`;
+
+export const Main = styled.main`
+  width: clamp(320px, 80%, 1170px);
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
